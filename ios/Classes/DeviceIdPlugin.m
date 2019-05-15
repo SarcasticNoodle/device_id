29424e42486adf0505eb124bdc8c89ab61dba58d
zuowenbo <zuowenbo@zhihu.com>
2019-05-16 00:03:19 +0800

    Fix Run error

--- ios/Classes/DeviceIdPlugin.m
+++ ios/Classes/DeviceIdPlugin.m
@@ -1,8 +1,22 @@
 #import "DeviceIdPlugin.h"
-#import <device_id/device_id-Swift.h>
+#import <Flutter/Flutter.h>
+//public class SwiftDeviceIdPlugin: NSObject, FlutterPlugin {
+//    public static func register(with registrar: FlutterPluginRegistrar) {
+//        let channel = FlutterMethodChannel(name: "device_id", binaryMessenger: registrar.messenger())
+//        let instance = SwiftDeviceIdPlugin()
+//        registrar.addMethodCallDelegate(instance, channel: channel)
+//    }
+//
+//    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+//        result(UIDevice.current.identifierForVendor?.uuidString)
+//    }
+//}
 
 @implementation DeviceIdPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftDeviceIdPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"device_id" binaryMessenger:registrar.messenger];
+    DeviceIdPlugin *instance = [[DeviceIdPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
 @end
